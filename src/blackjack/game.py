fd9dc6e4094f96e3667f870d669a00de4226d37f
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2022-11-13 16:39:03 -0500

    :sparkles: Add betting feature.
    Game now works completely as per the instructions. However, it is aweful
    and is not easily extended. Much refactoring is still needed.

--- src/blackjack/game.py
+++ src/blackjack/game.py
@@ -4,6 +4,7 @@ from blackjack.players import Dealer, Player
 
 class Blackjack:
     """Main Game Handler"""
+    MINIMUM_BET: float = 1
 
     def __init__(self, deck: Deck, player: Player, dealer: Dealer):
         self.deck = deck
@@ -13,16 +14,27 @@ class Blackjack:
     def start_game(self) -> None:
         while True:
             print("")
-            play_hand = input("Would you like to play a hand? ")
+            if self.player.balance < self.MINIMUM_BET:
+                print("You've ran out of money. "
+                      "Please restart this program to try again. Goodbye.")
+                break
+            play_hand = input(
+                f"You are starting with ${self.player.balance}. "
+                "Would you like to play a hand? "
+            )
             if play_hand == "yes":
                 self._play_round()
             elif play_hand == "no":
-                print("Okay, hope you come back soon!")
+                print(f"You've ended with ${self.player.balance}. "
+                      "Hope you come back soon! Goodbye.")
                 break
             else:
-                print("Invalid option.")
+                print("Invalid option. Type yes or no.")
 
     def _play_round(self) -> None:
+
+        bet = self.player.bet(self.MINIMUM_BET)
+
         self.deck.reset()
         self.deck.shuffle()
         self.player.hand.cards = []
@@ -37,42 +49,57 @@ class Blackjack:
         if self.player.has_blackjack:
             print(f"The dealer has: {self.dealer.hand}")
             if not self.dealer.has_blackjack:
-                print("Blackjack! You win!")
+                prize = bet * 1.5
+                self.player.balance += prize
+                print(f"Blackjack! You win ${prize}!")
             else:
-                print("You Tie.")
+                print("You Tie. Your bet has been returned.")
 
 
 
         try:
             self._move(self.player)
         except ValueError:
-            print("Your hand value is over 21 and you lose. :(")
+            self.player.balance -= bet
+            print(f"Your hand value is over 21 and you lose ${bet}. :(")
             return
 
         print(f"The dealer has: {self.dealer.hand}")
         try:
             self._move(self.dealer)
         except ValueError:
-            print("The dealer busts, you win! :)")
+            self.player.balance += bet
+            print(f"The dealer busts, you win ${bet}! :)")
             return
 
         if self.player.hand.value < self.dealer.hand.value:
-            print("The dealer wins, you lose. :(")
+            self.player.balance -= bet
+            print(f"The dealer wins, you lose ${bet}. :(")
         elif self.player.hand.value > self.dealer.hand.value:
-            print("You win! :)")
+            self.player.balance += bet
+            print(f"You win ${bet}! :)")
         else:
-            print("You Tie.")
+            print("You Tie. Your bet has been returned.")
 
     def _move(self, current_player: Player):
+        if current_player is self.player:
+            name = "You"
+            verb = "are"
+            end = ""
+        else:
+            name = "The dealer"
+            verb = "is"
+            end = "s"
+
         while True:
             if current_player.hand.value > 21:
                 raise ValueError(f"Busted! hand value is {current_player.hand.value}")
             move = current_player.make_move()
             if move == "hit":
                 new_card = self.deck.deal()
-                print(f"You are dealt: {new_card[0]}")
+                print(f"{name} {verb} dealt: {new_card[0]}")
                 current_player.hand.add_cards(new_card)
-                print(f"You now have: {current_player.hand}")
+                print(f"{name} now have: {current_player.hand}")
             else:
-                print(f"{current_player} stays.")
+                print(f"{name} stay{end}.")
                 break
--- src/blackjack/players.py
+++ src/blackjack/players.py
@@ -9,8 +9,9 @@ class Moves(str, Enum):
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, initial_balance: float = 500):
         self.hand = Hand()
+        self.balance = initial_balance
 
     def make_move(self):
         while True:
@@ -30,6 +31,19 @@ class Player:
     def has_blackjack(self) -> bool:
         return self.hand.value == 21 and len(self.hand) == 2
 
+    def bet(self, minimum_bet: float) -> float:
+        while True:
+            try:
+                bet = float(input("Place your bet: "))
+            except ValueError:
+                print("Please enter a valid number.")
+            else:
+                if bet > self.balance:
+                    print("You don't have sufficient funds!")
+                elif bet < minimum_bet:
+                    print(f"The minimum bet is ${minimum_bet}")
+                else:
+                    return bet
 
 
 class Dealer:
